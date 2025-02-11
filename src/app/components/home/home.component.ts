4f61850e9410670b89264290bdaec559ed80ad9b
RafaelProfMgz <154349177+RafaelProfMgz@users.noreply.github.com>
2025-02-11 18:12:17 -0400

    adicionado fade nos projetos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  isVisible = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+
+    const element = document.querySelector('.fade-element');
+    if (element) {
+      const position = element.getBoundingClientRect();
+      if (position.top < window.innerHeight) {
+        this.isVisible = true;
+      }
+    }
+  }
 }
